683dcd3a7c15bdfd7cfc789ea3e40b3825f5d145
SanaSunayenaShaik <122111737+SanaSunayenaShaik@users.noreply.github.com>
2023-07-09 20:19:28 +0530

    Delete technical.py

--- technical.py
+++
@@ -1,9 +0,0 @@
-#n = int(input())
-#for i in range(1, int(n**0.5)+1):
- #   if n%i == 0:
-  #      print(i, end='\n')
-   #     if i*i!=n:
-#            print(n//i,end = '\n')
-#print (-287>>3)
-p = [i for i in range (1,1000000001)]
-print(999999999 in p)
